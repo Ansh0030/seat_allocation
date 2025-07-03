fa4b8522f8d57acf134c22cc750e78029a83dd84
Ansh Kumar <ansh.kumar@mpccloudconsulting.com>
2025-07-03 11:27:26 +0530

    Remove Routing module from tab1

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import { Tab1PageRoutingModule } from './tab1-routing.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
-    FormsModule,
-    Tab1PageRoutingModule,
+    FormsModule
   ],
   declarations: [Tab1Page]
 })
