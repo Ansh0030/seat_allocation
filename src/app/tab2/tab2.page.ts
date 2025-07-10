e3228a175049efc97cebb8267b3ba71376eda6b7
Ansh Kumar <ansh.kumar@mpccloudconsulting.com>
2025-07-10 11:22:43 +0530

    ADded Changes on tab2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab2',
@@ -8,23 +9,27 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class Tab2Page {
 
-  myForm !: FormGroup;
+  myForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-    this.myForm = this.fb.group({
-      row: [{ value: '1', disabled: true }, Validators.required],
-      col: [{ value: '2', disabled: true }, Validators.required],
-      empcode: ['', [Validators.required]],
-      name: [{ value: 'Ansh', disabled: true }, Validators.required],
-      last: [{ value: '', disabled: true }]
+    this.route.queryParams.subscribe(params => {
+      const row = params['row'];
+      const col = params['col'];
+
+      this.myForm = this.fb.group({
+        row: [{ value: row || '', disabled: true }, Validators.required],
+        col: [{ value: col || '', disabled: true }, Validators.required],
+        empcode: ['', [Validators.required]],
+        name: [{ value: 'Ansh', disabled: true }, Validators.required],
+        last: [{ value: '', disabled: true }]
+      });
     });
   }
 
   onSubmit(form: FormGroup) {
     console.log(form.getRawValue());
-
+    this.router.navigate(['/tab1']);
   }
-
 }
