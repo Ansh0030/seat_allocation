0bfc5e6e8434df5095485dfc94ddfdf68201aad0
Ansh Kumar <ansh.kumar@mpccloudconsulting.com>
2025-07-10 11:04:41 +0530

    ADded Changes

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -30,7 +30,7 @@ export class TableComponent {
     if (!this.isBooked) {
       const alert = await this.alertController.create({
         header: 'Do you want to Allcoate this Table?',
-        message: `Row: ${this.rowNo}, Seat: ${this.colNo} (${this.seatLabel})`,
+        message: `Row:${this.rowNo}, Seat:${this.seatLabel} (${this.colNo})`,
         mode: 'ios',
         buttons: [
           {
