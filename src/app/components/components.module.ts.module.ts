38829851af6de76e1cf6cc322723246ba2bc2de4
Ansh Kumar <ansh.kumar@mpccloudconsulting.com>
2025-07-21 11:02:40 +0530

    Created Relative servies

--- src/app/components/components.module.ts.module.ts
+++ src/app/components/components.module.ts.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { TableComponent } from './table/table.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [TableComponent],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    FormsModule
   ],
   exports: [TableComponent]
 })
